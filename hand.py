9cdbbc12d085ffa229bd85e21721b451635c7d2e
Zakky <kycraft1164@gmail.com>
2022-08-08 21:14:53 +0700

    add

--- hand.py
+++ hand.py
@@ -7,7 +7,13 @@ import mouse
 cam = cv2.VideoCapture(0)
 mp_hands = mp.solutions.hands.Hands(max_num_hands=1)
 screen_w, screen_h = pyautogui.size()
-print(screen_w)
+
+def isPointsClose(two_points, axis):
+    if axis == 'x':
+        points_distance = int((two_points[0].x - two_points[1].x) * 100)
+    else:
+        points_distance = int((two_points[0].y - two_points[1].y) * 100)
+    return (points_distance >= -3 and points_distance <= 3)
 
 while True:
     _, frame = cam.read()
@@ -28,17 +34,17 @@ while True:
         # draw eye points
         for landmark_id, landmark in enumerate(hand_landmarks):
             # draw on particular landmark
-            if landmark_id == 5 or landmark_id == 4 or landmark_id == 12:
+            if landmark_id == 5 or landmark_id == 4 or landmark_id == 12 or landmark_id == 16:
                 x = int(landmark.x * frame_w)
                 y = int(landmark.y * frame_h)
                 draw_coordinates = (x,y)
 
                 if landmark_id == 5:
                     size = 15
-                    rgb = (0, 255, 255)
+                    rgb = (255, 255, 0)
                 else:
-                    size = 7
-                    rgb = (0, 0, 255)
+                    size = 10
+                    rgb = (0, 255, 255)
 
                 cv2.circle(frame, draw_coordinates, size, rgb)
 
@@ -50,15 +56,12 @@ while True:
                 cursor_coordinates = [cursor_x, cursor_y]
                 mouse.move(cursor_x, cursor_y)
 
-        index_finger_points = [hand_landmarks[12], hand_landmarks[4]]
-
-        finger_points_distance_y = int((index_finger_points[0].y - index_finger_points[1].y) * 100)
-        finger_points_distance_x = int((index_finger_points[0].x - index_finger_points[1].x) * 100)
-        is_fingerPointsDistanceY_close = finger_points_distance_y >= -4 and finger_points_distance_y <= 4
-        is_fingerPointsDistanceX_close = finger_points_distance_x >= -4 and finger_points_distance_x <= 4
-        
-        if is_fingerPointsDistanceY_close and is_fingerPointsDistanceX_close: # they are so close
+        left_click_points = [hand_landmarks[4], hand_landmarks[12]]
+        right_click_points = [hand_landmarks[4], hand_landmarks[16]]
+        if isPointsClose(left_click_points, axis='x') and isPointsClose(left_click_points, axis='y'): # they are so close
             mouse.click('left')
+        if isPointsClose(right_click_points, axis='x') and isPointsClose(right_click_points, axis='y'): # they are so close
+            mouse.click('right')
 
     # create window
     cv2.imshow('Eye Controlled Mouse', frame)
